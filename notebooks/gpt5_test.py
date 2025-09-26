974f690dc0db86546935a8de40f98339f3184883
EnkrateiaLucca <lucasbnsoares@hotmail.com>
2025-09-26 01:27:09 +0100

    First commit

---
+++ notebooks/gpt5_test.py
@@ -0,0 +1,12 @@
+from openai import OpenAI
+client = OpenAI()
+
+response = client.responses.create(
+    model="gpt-5",
+    input="How much gold would it take to coat the Statue of Liberty in a 1mm layer?",
+    reasoning={
+        "effort": "minimal"
+    }
+)
+
+print(response)
